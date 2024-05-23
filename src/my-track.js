492f4aa3fb5896ac246ce3abd3a77e756f2154f5
GabrielGarciaRodri <145493679+GabrielGarciaRodri@users.noreply.github.com>
2024-05-23 05:32:53 -0500

    fix: :bug: Bug fixed on track part

--- src/my-track.js
+++ src/my-track.js
@@ -22,7 +22,14 @@ export class myTrack extends LitElement{
         'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
       }
     };
-  
+    try {
+      const response = await fetch(url, options);
+      const result = await response.json();
+      this.songs = result.data.artist.discography.singles.items;
+      this.requestUpdate();
+    } catch (error) {
+        console.error('Error fetching songs:', error);
+      }
     
   }
 
@@ -54,64 +61,64 @@ export class myTrack extends LitElement{
 }
 
   static styles = css`
-  .padre{
-    height: 150px;
-    overflow-y: scroll;
-  }
-  .card {
-    margin: 10px;
-    padding: 0;
-}
+    .padre{
+      height: 150px;
+      overflow-y: scroll;
+    }
+    .card {
+      margin: 10px;
+      padding: 0;
+    }
 
-.cards{
-    background: white;
-    height: 50px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    overflow: hidden; 
-    margin-bottom: 10px
-    overflow-y: scroll;
-}
+    .cards{
+      background: white;
+      height: 50px;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      overflow: hidden; 
+      margin-bottom: 10px
+      overflow-y: scroll;
+    }
 
-.cards h3{
-  margin: 5px;
-  font-size: 15px ;
-}
+    .cards h3{
+      margin: 5px;
+      font-size: 15px ;
+    }
 
-.cards h5{
-  margin: 0;
-  color: rgb(124, 124, 124);
-}
+    .cards h5{
+      margin: 0;
+      color: rgb(124, 124, 124);
+    }
 
-.cards button{
-  background: none;
-  border: none;
-}
+    .cards button{
+      background: none;
+      border: none;
+    }
 
-.card button:hover{
-  cursor: pointer;
-}
+    .card button:hover{
+      cursor: pointer;
+    }
 
-.cards .cards_info{
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-}
+    .cards .cards_info{
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+    }
 
-.cards .cards_info .img{
-  height: 50px;
-  width: 50px
-}
+    .cards .cards_info .img{
+      height: 50px;
+      width: 50px
+    }
 
-.cards .cards_info .img img{
-  height: 50px;
-  width: 50px;
-  object-fit: cover;
-}
+    .cards .cards_info .img img{
+      height: 50px;
+      width: 50px;
+      object-fit: cover;
+    }
 
-.cards .minutes_and_date{
-  padding-right: 10px
-}
+    .cards .minutes_and_date{
+      padding-right: 10px
+    }
   `
 }   
